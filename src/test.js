657bc9671eff26215cdbc07c85d5e5988bc392df
zwigglers <zwigglers@gmail.com>
2017-08-10 18:06:21 +0800

    Initial commit
    Signed-off-by: zwigglers <zwigglers@gmail.com>

---
+++ src/test.js
@@ -0,0 +1,44 @@
+import AWS from "aws-sdk";
+import S3Stream from './S3Stream';
+
+AWS.config.update({
+	accessKeyId: "<YOUR_ACCESS_KEY>",
+	secretAccessKey: '<YOUR_SECRET_ACCESS>',
+	region: "<REGION>"
+});
+let s3 = new AWS.S3();
+
+const BUCKET = "<BUCKET_NAME>";
+const KEY = "<KEY_NAME>";
+
+let s3stream = new S3Stream(s3);
+
+let string = "Some string";
+let url = 'http://www.google.com/';
+
+let uploadParams = {
+	Bucket: BUCKET,
+	Key: KEY,
+	ContentType: "text/plain"
+};
+
+let downloadParams = {
+	Bucket: BUCKET,
+ 	Key: KEY
+};
+
+s3stream.stringToS3(string, uploadParams).then(function() {
+	console.log('Upload Success');
+}).catch(function(err) {
+	console.log('Upload Error', err);
+});
+
+setTimeout(function() {
+
+	s3stream.stringFromS3(downloadParams).then(function(string) {
+		console.log('Download Success', string);
+	}).catch(function(err) {
+		console.log('Download Error', err);
+	});
+
+}, 5000);
